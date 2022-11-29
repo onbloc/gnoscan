1a6faee2f6698bce955127c0087456222440dece
jinoosss <jwchoi@onbloc.xyz>
2022-11-29 17:14:09 +0900

    feat: create area chart component

---
+++ src/components/ui/chart/index.ts
@@ -0,0 +1,24 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+);
+
+export * from './area-chart';
