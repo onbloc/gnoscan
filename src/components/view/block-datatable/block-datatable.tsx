062c8c26e4fb0db363e91cbcbf4cf7beb91d34e8
jinoosss <jwchoi@onbloc.xyz>
2022-12-01 16:22:56 +0900

    feat: update block datatable

--- src/components/view/block-datatable/block-datatable.tsx
+++ src/components/view/block-datatable/block-datatable.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Datatable, {DatatableHeader} from '@/components/ui/datatable';
 import useTheme from '@/common/hooks/use-theme';
 import Link from 'next/link';
@@ -8,85 +8,62 @@ import {getDateDiff} from '@/common/utils/date-util';
 import {textEllipsis} from '@/common/utils/string-util';
 
 const PADDING = 32;
-const HEADER_OPTION: Array<DatatableHeader.Header<Block>> = [
-  {
-    key: 'block_hash',
-    name: 'Block Hash',
-    width: `${211 + PADDING}px`,
-    colorName: 'blue',
-    renderOption: (value, data) => (
-      <Link href={`/blocks/${data.block_hash}`}>{textEllipsis(value, 8)}</Link>
-    ),
-  },
-  {
-    key: 'height',
-    name: 'Height',
-    width: `${89 + PADDING}px`,
-    colorName: 'blue',
-    renderOption: (value, data) => <Link href={`/blocks/${data.block_hash}`}>{value}</Link>,
-  },
-  {
-    key: 'time',
-    name: 'Time',
-    width: `${194 + PADDING}px`,
-    renderOption: value => `${getDateDiff(value)}`,
-  },
-  {
-    key: 'tx_count',
-    name: 'Tx Count',
-    width: `${134 + PADDING}px`,
-  },
-  {
-    key: 'proposer',
-    name: 'Proposer',
-    width: `${194 + PADDING}px`,
-    colorName: 'blue',
-    renderOption: (value, data) => <Link href={`/blocks/${data.block_hash}`}>{value}</Link>,
-  },
-  {
-    key: 'total_fees',
-    name: 'Total Fees',
-    width: `${131 + PADDING}px`,
-    renderOption: value => `${value} GNOT`,
-  },
-];
 
-interface Block {
-  block_hash: string;
-  height: number;
-  time: string;
-  tx_count: number;
-  proposer: string;
-  total_fees: number;
-}
-
-const BlockDatatable = () => {
+const BlockDatatable = <T extends {[key in string]: any}>({datas}: {datas: Array<T>}) => {
   const [theme] = useTheme();
-  const [blockData, setBlockData] = useState<Array<Block>>([]);
 
-  useEffect(() => {
-    fetchBlocks();
-  }, []);
-
-  const fetchBlocks = () => {
-    try {
-      fetch('http://3.218.133.250:7677/v0/list/blocks')
-        .then(res => res.json())
-        .then(res => setBlockData(res));
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const createHeaders = (): Array<DatatableHeader.Header<T>> => [
+    {
+      key: 'block_hash',
+      name: 'Block Hash',
+      width: `${211 + PADDING}px`,
+      colorName: 'blue',
+      renderOption: (value, data) => (
+        <Link href={`/blocks/${data.block_hash}`}>{textEllipsis(value, 8)}</Link>
+      ),
+    },
+    {
+      key: 'height',
+      name: 'Height',
+      width: `${89 + PADDING}px`,
+      colorName: 'blue',
+      renderOption: (value, data) => <Link href={`/blocks/${data.block_hash}`}>{value}</Link>,
+    },
+    {
+      key: 'time',
+      name: 'Time',
+      width: `${194 + PADDING}px`,
+      renderOption: value => `${getDateDiff(value)}`,
+    },
+    {
+      key: 'tx_count',
+      name: 'Tx Count',
+      width: `${134 + PADDING}px`,
+    },
+    {
+      key: 'proposer',
+      name: 'Proposer',
+      width: `${194 + PADDING}px`,
+      colorName: 'blue',
+      renderOption: (value, data) => <Link href={`/blocks/${data.block_hash}`}>{value}</Link>,
+    },
+    {
+      key: 'total_fees',
+      name: 'Total Fees',
+      width: `${131 + PADDING}px`,
+      renderOption: value => `${value} GNOT`,
+    },
+  ];
 
   return (
     <Datatable
-      headers={HEADER_OPTION.map(item => {
+      headers={createHeaders().map(item => {
         return {
           ...item,
           themeMode: theme,
         };
       })}
-      datas={blockData}
+      datas={datas}
     />
   );
 };
--- src/pages/blocks/index.tsx
+++ src/pages/blocks/index.tsx
@@ -1,9 +1,34 @@
 import Text from '@/components/ui/text';
 import BlockDatatable from '@/components/view/block-datatable/block-datatable';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
+interface BlockData {
+  block_hash: string;
+  height: number;
+  time: string;
+  tx_count: number;
+  proposer: string;
+  total_fees: number;
+}
+
 const Block = () => {
+  const [blocks, setBlocks] = useState<Array<BlockData>>([]);
+
+  useEffect(() => {
+    fetchBlocks();
+  }, []);
+
+  const fetchBlocks = () => {
+    try {
+      fetch('http://3.218.133.250:7677/v0/list/blocks')
+        .then(res => res.json())
+        .then(res => setBlocks(res));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <Container>
       <div className="inner-layout">
@@ -11,7 +36,7 @@ const Block = () => {
           <Text type="h2" margin={'0 0 24px 0'} color="primary">
             {'Blocks'}
           </Text>
-          <BlockDatatable />
+          <BlockDatatable datas={blocks} />
         </Wrapper>
       </div>
     </Container>
