0f53311de34e196ad4da42a2d4f8fe1e52b215ab
hyojinDev <gywls3394@gmail.com>
2022-12-02 11:12:16 +0900

    modified: Main page padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,12 @@ import MainCard from '@/components/view/main-card/main-card';
 import MainRealm from '@/components/view/main-realm/main-realm';
 import MainTransactionNews from '@/components/view/main-transaction-news/main-transaction-news';
 import MainActiveList from '@/components/view/main-active-list';
+import {eachMedia} from '@/common/hooks/use-media';
 
 const Home: React.FC = () => {
+  const media = eachMedia();
   return (
-    <Wrapper>
+    <Wrapper media={media}>
       <div className="inner-layout">
         <MainCard />
         <MainActiveList />
@@ -18,10 +20,10 @@ const Home: React.FC = () => {
   );
 };
 
-const Wrapper = styled.main`
+const Wrapper = styled.main<{media: string}>`
   width: 100%;
   flex: 1;
-  padding: 48px 0px;
+  padding: ${({media}) => (media === 'desktop' ? '48px 0px' : '24px 0px')};
 `;
 
 const SecondLine = styled.div`
