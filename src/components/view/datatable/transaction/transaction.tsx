65f48232b5f8821ce5c1d1630d8462870f358bef
jinoosss <jwchoi@onbloc.xyz>
2022-12-15 17:42:08 +0900

    feat: open transaction result function when develop

--- src/components/view/datatable/transaction/transaction.tsx
+++ src/components/view/datatable/transaction/transaction.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Datatable, {DatatableOption} from '@/components/ui/datatable';
 import useTheme from '@/common/hooks/use-theme';
 import Link from 'next/link';
@@ -55,6 +55,30 @@ export const TransactionDatatable = () => {
     pageable: true,
   });
   useLoading({finished});
+  const [development, setDevelopment] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydownEvent);
+    window.addEventListener('keyup', handleKeyupEvent);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydownEvent);
+      window.removeEventListener('keyup', handleKeyupEvent);
+    };
+  }, []);
+
+  const handleKeydownEvent = (event: KeyboardEvent) => {
+    if (event.code === 'Backquote') {
+      setDevelopment(true);
+      setTimeout(() => setDevelopment(false), 500);
+    }
+  };
+
+  const handleKeyupEvent = (event: KeyboardEvent) => {
+    if (event.code === 'Backquote') {
+      setDevelopment(false);
+    }
+  };
 
   const getTransactions = (): Array<TransactionData> => {
     if (!data) {
@@ -83,7 +107,14 @@ export const TransactionDatatable = () => {
       .name('Tx Hash')
       .width(210)
       .colorName('blue')
-      .renderOption((value, data) => <DatatableItem.TxHash txHash={value} success={data.success} />)
+      .renderOption((value, data) => (
+        <DatatableItem.TxHash
+          txHash={value}
+          success={data.success}
+          development={development}
+          height={data.block}
+        />
+      ))
       .tooltip(TOOLTIP_TX_HASH)
       .build();
   };
