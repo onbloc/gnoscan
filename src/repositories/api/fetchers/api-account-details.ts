adfbaf076d622f1e90e7b6bf73a95ea3e6608c83
hyojinDev <gywls3394@gmail.com>
2023-05-12 16:45:02 +0900

    feat: [GNS-4] Add GRC20 token balance on Account Details page

--- src/repositories/api/fetchers/api-account-details.ts
+++ src/repositories/api/fetchers/api-account-details.ts
@@ -1,8 +1,8 @@
 import axios, {AxiosResponse} from 'axios';
-import {API_URI, API_VERSION} from '@/common/values/constant-value';
+import {API_V2_URI, API_V2_VERSION} from '@/common/values/constant-value';
 
 export const getAccountDetails = async (
   address: string | any,
 ): Promise<AxiosResponse<any, any>> => {
-  return await axios.get(API_URI + API_VERSION + `/account/detail/${address}`);
+  return await axios.get(API_V2_URI + API_V2_VERSION + `/account/detail/${address}`);
 };
