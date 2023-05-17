fe2f2d20d2db68d2496a65e313159cd6d468703b
이효진 <hyojin@ihyojin-ui-MacBookPro.local>
2023-05-17 13:15:03 +0900

    feat: [GNS-8] Add GRC20 token data in Transaction Details page

--- src/repositories/api/fetchers/api-transaction-details.ts
+++ src/repositories/api/fetchers/api-transaction-details.ts
@@ -4,5 +4,5 @@ import {API_URI, API_VERSION} from '@/common/values/constant-value';
 export const getTransactionDetails = async (
   hash: string | any,
 ): Promise<AxiosResponse<any, any>> => {
-  return await axios.get(API_URI + API_VERSION + `/tx/${hash}`);
+  return await axios.get('https://dev-api.gnoscan.io/v2' + `/tx/${hash}`);
 };
