3756fe7063525ba8bd2e040ecb7cd124b1c18e91
이효진 <hyojin@ihyojin-ui-MacBookPro.local>
2023-05-22 10:23:43 +0900

    fix: accounts-details page API url

--- src/repositories/api/fetchers/api-account-details.ts
+++ src/repositories/api/fetchers/api-account-details.ts
@@ -4,5 +4,5 @@ import {API_V2_URI, API_V2_VERSION} from '@/common/values/constant-value';
 export const getAccountDetails = async (
   address: string | any,
 ): Promise<AxiosResponse<any, any>> => {
-  return await axios.get(API_V2_URI + API_V2_VERSION + `/account/detail/${address}`);
+  return await axios.get('https://dev-api.gnoscan.io/v2' + `/account/detail/${address}`);
 };
