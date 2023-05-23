7524e2be6610bcdf310f42173eb1e04817ae8ed9
이효진 <hyojin@ihyojin-ui-MacBookPro.local>
2023-05-23 18:05:48 +0900

    feat: token resource

--- src/repositories/api/selector/select-account-details.ts
+++ src/repositories/api/selector/select-account-details.ts
@@ -1,7 +1,15 @@
-import {toGnot} from '@/common/utils/gnot-util';
+import {AccountDetailsModel, AssetsDataType} from '@/models/account-details-model';
+import {allTokenSelector} from './select-meta-token';
+import {TokenModel} from '../models/meta-token-model';
 
-export const accountDetailSelector = (data: any) => {
+export const accountDetailSelector = (
+  data: AccountDetailsModel,
+  tokens: TokenModel[],
+): AccountDetailsModel => {
+  console.log('Tokens ; ', tokens);
+  const assets = allTokenSelector(data.assets, tokens);
   return {
     ...data,
+    assets,
   };
 };
