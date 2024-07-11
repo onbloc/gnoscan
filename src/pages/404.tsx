b96cebf491db5983152a55040d868e0b50c04892
jinoosss <jwchoi@onbloc.xyz>
2024-07-11 20:46:14 +0900

    fix: adjust 404 page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import NotFound from '@/components/view/not-found/not-found';
+import styled from 'styled-components';
 import {useRouter} from 'next/router';
+
+import NotFound from '@/components/view/not-found/not-found';
 
 const NotFoundPage = () => {
   const {asPath} = useRouter();
 
-  return <NotFound keyword={asPath} />;
+  return (
+    <Wrapper>
+      <div className="inner-layout">
+        <NotFound keyword={asPath} />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  width: 100%;
+  flex: 1;
+`;
 
 export default NotFoundPage;
