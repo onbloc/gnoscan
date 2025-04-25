4d24c55b165ab6b969e05c1f8ad148cc9d7e58d4
yjin <124853691+tfrg@users.noreply.github.com>
2025-04-25 22:18:55 +0900

    refactor: [GNS-255] UI Layer for Realms (#142)

--- src/pages/realms/index.tsx
+++ src/pages/realms/index.tsx
@@ -1,42 +1,12 @@
-import useLoading from "@/common/hooks/use-loading";
-import Text from "@/components/ui/text";
-import { RealmDatatable } from "@/components/view/datatable/realm";
-import LoadingPage from "@/components/view/loading/page";
 import React from "react";
-import styled from "styled-components";
 
-const Realms = () => {
-  const { loading } = useLoading();
+import RealmsLayout from "@/layouts/realms/RealmsLayout";
+const RealmListContainer = React.lazy(() => import("@/containers/realms/realm-list-container/RealmListContainer"));
 
+export default function Page() {
   return (
-    <Container>
-      <div className="inner-layout">
-        <LoadingPage visible={loading} />
-        <Wrapper visible={!loading}>
-          <Text type="h2" margin={"0 0 24px 0"} color="primary">
-            {"Realms"}
-          </Text>
-          <RealmDatatable />
-        </Wrapper>
-      </div>
-    </Container>
+    <>
+      <RealmsLayout realmList={<RealmListContainer />} />
+    </>
   );
-};
-
-const Container = styled.main`
-  width: 100%;
-  flex: 1;
-`;
-
-const Wrapper = styled.div<{ visible?: boolean }>`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 40px 0;
-  padding: 24px;
-  background-color: ${({ theme }) => theme.colors.surface};
-  border-radius: 10px;
-  ${({ visible }) => !visible && "display: none;"}
-`;
-
-export default Realms;
+}
