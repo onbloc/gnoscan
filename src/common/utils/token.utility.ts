17ad41630f3525ec52bff9591835087a218166f1
yjin <devyzyn@gmail.com>
2025-05-14 14:29:44 +0900

    feat: [GNS-286] formatTokenDecimal utility function

--- src/common/utils/token.utility.ts
+++ src/common/utils/token.utility.ts
@@ -1,6 +1,18 @@
+import BigNumber from "bignumber.js";
+
 export function parseTokenAmount(tokenAmount: string, denomination = "ugnot"): number {
   const pattern = new RegExp(`^(\\d+)${denomination}$`);
   const match = tokenAmount.match(pattern);
 
   return match ? parseInt(match[1], 10) : 0;
+}
+
+export function formatTokenDecimal(amount: string | number, decimals = 6): string {
+  const amountToBigNumber = new BigNumber(amount);
+
+  if (amountToBigNumber.isNaN() || !amountToBigNumber.isFinite()) {
+    return "0";
+  }
+
+  return amountToBigNumber.shiftedBy(-decimals).toString(10);
 }
