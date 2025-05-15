ded47d2cb8328c4b8b6348ca95bf9c94609bf357
yjin <devyzyn@gmail.com>
2025-05-15 19:36:37 +0900

    chore: [GNS-296] BuildError

--- src/common/utils/block.utility.test.ts
+++ src/common/utils/block.utility.test.ts
@@ -2,13 +2,13 @@ import { formatDisplayBlockHeight } from "./block.utility";
 import { GNO_BLOCK_CONSTANTS } from "../values/gno.constant";
 
 describe("formatDisplayBlockHeight", () => {
-  it('should return "-" when value is null', () => {
+  it("should return '-' when value is null", () => {
     expect(formatDisplayBlockHeight(null)).toBe("-");
     expect(formatDisplayBlockHeight(undefined)).toBe("-");
     expect(formatDisplayBlockHeight("")).toBe("-");
   });
 
-  it('should return GENESIS constant when value is "0"', () => {
+  it("should return GENESIS constant when value is '0'", () => {
     expect(formatDisplayBlockHeight("0")).toBe(GNO_BLOCK_CONSTANTS.GENESIS);
     expect(formatDisplayBlockHeight(0)).toBe(GNO_BLOCK_CONSTANTS.GENESIS);
   });
