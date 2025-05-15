6c55306b520d12fdcb199af76a1e25cb72b3f83b
yjin <devyzyn@gmail.com>
2025-05-15 19:30:42 +0900

    test: [GNS-296] formatDisplayBlockHeight

--- src/common/utils/block.utility.ts
+++ src/common/utils/block.utility.ts
@@ -1,8 +1,13 @@
 import { GNO_BLOCK_CONSTANTS } from "../values/gno.constant";
 
 export const formatDisplayBlockHeight = (height: number | string | undefined | null): string => {
-  if (height == 0) return GNO_BLOCK_CONSTANTS.GENESIS;
   if (height == null) return "-";
+
+  const stringHeight = String(height).trim();
+
+  if (!stringHeight || isNaN(Number(stringHeight))) return "-";
+
+  if (stringHeight === "0") return GNO_BLOCK_CONSTANTS.GENESIS;
 
   return height.toString();
 };
