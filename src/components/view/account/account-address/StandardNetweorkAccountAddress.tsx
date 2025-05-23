d4bfae953607e06273fac4d2abb6229ad76485ab
yjin <devyzyn@gmail.com>
2025-05-23 11:40:02 +0900

    feat:[GNS-316] username for AccountDetails

--- src/components/view/account/account-address/StandardNetweorkAccountAddress.tsx
+++ src/components/view/account/account-address/StandardNetweorkAccountAddress.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Link from "next/link";
 
 import { useGetAccountByAddress } from "@/common/react-query/account/api/use-get-account-by-address";
-import { useNetwork } from "@/common/hooks/use-network";
 import { DEVICE_TYPE } from "@/common/values/ui.constant";
+import { useNetwork } from "@/common/hooks/use-network";
 
 import * as S from "./AccountAddress.styles";
 import Text from "@/components/ui/text";
 import IconCopy from "@/assets/svgs/icon-copy.svg";
 import IconLink from "@/assets/svgs/icon-link.svg";
 import AccountAddressSkeleton from "./AccountAddressSkeleton";
+import { Divider } from "@/components/ui/divider/Divider";
 
 interface AccountAddressProps {
   breakpoint: DEVICE_TYPE;
@@ -18,7 +19,13 @@ interface AccountAddressProps {
 }
 
 const StandardNetworkAccountAddress = ({ breakpoint, isDesktop, address }: AccountAddressProps) => {
-  const { isLoading, isFetched } = useGetAccountByAddress(address);
+  const { data, isLoading, isFetched } = useGetAccountByAddress(address);
+  const { getUrlWithNetwork } = useNetwork();
+
+  const username: string | null = React.useMemo(() => {
+    if (!data?.data || !data?.data?.name) return null;
+    return data.data.name;
+  }, [data?.data.name]);
 
   if (isLoading || !isFetched) {
     return <AccountAddressSkeleton isDesktop={isDesktop} />;
@@ -31,14 +38,20 @@ const StandardNetworkAccountAddress = ({ breakpoint, isDesktop, address }: Accou
       </Text>
       <S.Box isDesktop={isDesktop}>
         <S.AccountWrapper>
-          <S.ContentWrapper>
+          <S.ContentWrapper isDesktop={isDesktop}>
             <S.Content type="p4" color="primary">
               {address}
               <S.CopyTooltip content="Copied!" trigger="click" copyText={address || ""}>
                 <IconCopy />
               </S.CopyTooltip>
-              {false && <UsernameDependentComponent breakpoint={breakpoint} userName={""} userUrl={""} />}
             </S.Content>
+            {username && (
+              <UsernameDependentComponent
+                breakpoint={breakpoint}
+                userName={username}
+                userUrl={getUrlWithNetwork(`/account/${address}`)}
+              />
+            )}
           </S.ContentWrapper>
         </S.AccountWrapper>
       </S.Box>
@@ -53,9 +66,13 @@ interface UsernameDependentComponentProps {
 }
 
 const UsernameDependentComponent = React.memo(({ breakpoint, userName, userUrl }: UsernameDependentComponentProps) => {
+  const isDesktop = breakpoint === DEVICE_TYPE.DESKTOP;
+
   return (
     <>
-      <S.ContentWrapper>
+      <S.ContentWrapper isDesktop={isDesktop}>
+        {/* <div style={{ width: 1, height: 18, border: "none", borderLeft: "1px solid black" }} /> */}
+        <Divider size={1} length={18} orientation="vertical" />
         <Link href={userUrl || ""} target="_blank" rel="noreferrer">
           <S.Username type="p4" color="blue" breakpoint={breakpoint}>
             {userName}
