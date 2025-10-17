6edb04ab9f485f33a60188e6dced2cd903b9bace
yjin <devyzyn@gmail.com>
2025-10-17 18:00:35 +0900

    feat: [GNS-364] Update GnoEvent types

--- src/types/event-types.ts
+++ src/types/event-types.ts
@@ -1,12 +1,13 @@
 export const EVENT_TYPES = {
-  GNO_EVENT: "/tm.GnoEvent",
+  GNO_EVENT: "/tm.Event",
   STORAGE_DEPOSIT_EVENT: "/tm.StorageDepositEvent",
+  STORAGE_UNLOCK_EVENT: "/tm.StorageUnlockEvent",
 } as const;
 
 export type EventType = (typeof EVENT_TYPES)[keyof typeof EVENT_TYPES];
 
 export interface GnoEventData {
-  "@type": "/tm.GnoEvent";
+  "@type": "/tm.Event";
   type: string;
   attrs: Array<{ key: string; value: string }>;
   pkg_path: string;
@@ -17,5 +18,13 @@ export interface StorageDepositEventData {
   "@type": "/tm.StorageDepositEvent";
   bytes_delta: string;
   fee_delta: string;
+  pkg_path: string;
+}
+
+export interface StorageUnlockEventData {
+  "@type": "/tm.StorageDepositEvent";
+  bytes_delta: string;
+  fee_refund: string;
+  refund_withheld: boolean;
   pkg_path: string;
 }
