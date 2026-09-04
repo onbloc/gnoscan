b203eb34785dc13d2157ef59b0350e72c7344af3
Aron Park <arondev1007@gmail.com>
2026-09-04 17:19:03 +0900

    fix(token): disable retry on token-meta lookup

--- src/common/react-query/token/api/use-get-token-meta-by-path.ts
+++ src/common/react-query/token/api/use-get-token-meta-by-path.ts
@@ -17,6 +17,12 @@ export const useGetTokenMetaByPath = (
     apiTokenRepository,
     API_REPOSITORY_KEY.TOKEN_REPOSITORY,
     repository => repository.getTokenMetaByPath(path),
-    { ...options, enabled: !!path, cacheTime: 60 * 60 * 1000, staleTime: 60 * 60 * 1000 },
+    {
+      enabled: !!path,
+      retry: false,
+      cacheTime: 60 * 60 * 1000,
+      staleTime: 60 * 60 * 1000,
+      ...options,
+    },
   );
 };
