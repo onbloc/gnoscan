d9ab87f9b4115beebd0dc9978d790c20aa3d6113
Dongwon <74406335+dongwon8247@users.noreply.github.com>
2026-09-16 00:32:39 +0900

    Update src/components/view/realm/realm-summary/StandardNetworkRealmSummary.tsx

--- src/components/view/realm/realm-summary/StandardNetworkRealmSummary.tsx
+++ src/components/view/realm/realm-summary/StandardNetworkRealmSummary.tsx
@@ -77,7 +77,7 @@ const notYetEnabledBadgeStyle = css`
 const NotYetEnabledBadge = () => (
   <Badge margin="0" cssExtend={notYetEnabledBadgeStyle}>
     <span className="not-yet-enabled-content">
-      <Text type="p4" color="white" fontWeight={600}>
+      <Text type="p4" color="white" fontWeight={400}>
         Not Yet Enabled
       </Text>
       <FloatingTooltip content={TOOLTIP_NOT_YET_ENABLED} className="not-yet-enabled-tooltip">
