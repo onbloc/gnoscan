171e0d409c334e81e32c9d8cebe9f96819246107
Aron Park <arondev1007@gmail.com>
2026-09-17 18:24:10 +0900

    feat: add token resource link to tokens page (#271)

--- src/layouts/tokens/TokensLayout.tsx
+++ src/layouts/tokens/TokensLayout.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import * as S from "./TokensLayout.styles";
 import { PageTitle } from "@/components/view/common/page-title/PageTitle";
+import IconLink from "@/assets/svgs/icon-link.svg";
+import Text from "@/components/ui/text";
+
+const GNO_TOKEN_RESOURCE_URL = process.env.NEXT_PUBLIC_GNO_TOKEN_RESOURCE_URL?.trim();
 
 interface TokensLayoutProps {
   tokenList: React.ReactNode;
@@ -12,7 +16,17 @@ const TokensLayout = ({ tokenList }: TokensLayoutProps) => {
     <S.Container>
       <S.InnerLayout>
         <S.Wrapper>
-          <PageTitle title="Tokens" type="h2" />
+          <S.TitleWrap>
+            <PageTitle title="Tokens" type="h2" />
+            {GNO_TOKEN_RESOURCE_URL && (
+              <S.TokenResourceLink href={GNO_TOKEN_RESOURCE_URL} target="_blank" rel="noreferrer">
+                <Text type="p4" className="ellipsis">
+                  Gno.land Token Resources
+                </Text>
+                <IconLink className="icon-link" />
+              </S.TokenResourceLink>
+            )}
+          </S.TitleWrap>
           {tokenList}
         </S.Wrapper>
       </S.InnerLayout>
