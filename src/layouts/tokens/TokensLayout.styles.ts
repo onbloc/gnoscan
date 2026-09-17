12e29f4cd430a58bb7992100242bdd0cbb752436
Aron Park <arondev1007@gmail.com>
2026-09-17 17:55:39 +0900

    feat: add token resource link to tokens page

--- src/layouts/tokens/TokensLayout.styles.ts
+++ src/layouts/tokens/TokensLayout.styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { innerLayoutCss } from "@/styles/css/inner-layout";
+import { LinkWrapper } from "@/components/ui/detail-page-common-styles";
 
 export const Container = styled.main`
   width: 100%;
@@ -22,4 +23,27 @@ export const Wrapper = styled.div`
   border-radius: 10px;
 
   background-color: ${({ theme }) => theme.colors.surface};
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 8px;
+  }
+`;
+
+export const TokenResourceLink = styled(LinkWrapper)`
+  align-self: center;
+  flex-shrink: 0;
+
+  @media (max-width: 767px) {
+    align-self: flex-start;
+  }
 `;
